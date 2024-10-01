10abb3e8f60f55436b6c5dbd47b7435301e72ee4
Sebastvin <Sebastvin@gmail.com>
2024-10-01 21:22:49 +0200

    Add base User model

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+with app.app_context():
+    db.create_all()
