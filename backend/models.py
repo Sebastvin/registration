5c95b193737c2951a3c8c83bc5063c578a3a5494
Sebastvin <Sebastvin@gmail.com>
2024-10-02 21:59:11 +0200

    Add is_organiser field

--- backend/models.py
+++ backend/models.py
@@ -5,3 +5,4 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    is_organiser = db.Column(db.Boolean, default=False, nullable=False)
