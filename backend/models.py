8bdd02ef0b417a76e34061bcc8c4a8428d2fed3a
Sebastvin <Sebastvin@gmail.com>
2024-10-02 23:27:27 +0200

    Add rest fields needed to User model

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,11 @@
 from . import db
+from enum import Enum
+from sqlalchemy import Enum as SQLAlchemyEnum
+
+
+class MealType(Enum):
+    VEGETARIAN = "vegetarian"
+    MEAT = "meat"
 
 
 class User(db.Model):
@@ -6,3 +13,4 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     is_organiser = db.Column(db.Boolean, default=False, nullable=False)
+    meal_preference = db.Column(SQLAlchemyEnum(MealType))
