b62a6fccd0cacaab6504fd8fc936fb07b61257b4
Sebastvin <Sebastvin@gmail.com>
2024-10-03 19:34:14 +0200

    Allow get user by id

--- backend/user_management.py
+++ backend/user_management.py
@@ -31,3 +31,28 @@ def get_users():
         ),
         200,
     )
+
+
+@user_management.route("/users/<int:user_id>", methods=["GET"])
+def get_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return (
+        jsonify(
+            {
+                "id": user.id,
+                "email": user.email,
+                "is_organiser": user.is_organiser,
+                "meal_preference": user.meal_preference.value
+                if user.meal_preference
+                else None,
+                "participation_start_time": user.participation_start_time.isoformat()
+                if user.participation_start_time
+                else None,
+                "participation_end_time": user.participation_end_time.isoformat()
+                if user.participation_end_time
+                else None,
+                "meals": [meal.meal_time.value for meal in user.meals],
+            }
+        ),
+        200,
+    )
